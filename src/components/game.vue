b7a0dd46203c28b6e9a4dd3130aea4a6f70808c7
v_hovorun <volodymyr@nayaone.com>
2023-03-08 19:58:50 +0200

    Initial commit

---
+++ src/components/game.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-card>
+    <v-text-field
+      v-model="sizeX"
+      label="Please enter size of x axis"
+      @input="redrawSquares"
+    />
+
+    <v-text-field
+      v-model="sizeY"
+      label="Please enter size of y axis"
+      @input="redrawSquares"
+    />
+  </v-card>
+
+  <div
+    class="grid-container"
+    :style="{ gridTemplateColumns: `repeat(${sizeX}, 36px)` }"
+  >
+    <div
+      v-for="(square, index) in squares"
+      :key="index"
+      :class="['grid-square', { 'blue': square.isBlue }]"
+      @mouseover="toggleSquareColor(square)"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  reactive,
+  ref,
+} from 'vue';
+
+const sizeX = ref(2);
+const sizeY = ref(2);
+
+const toggleSquareColor = (square: { isBlue: boolean }) => {
+  square.isBlue = !square.isBlue;
+};
+
+const squares = reactive([...Array(sizeX.value * sizeY.value)].map(() => ({ isBlue: false })));
+
+const redrawSquares = () => {
+  squares.splice(
+    0,
+    squares.length,
+    ...[...Array(sizeX.value * sizeY.value)].map(() => ({ isBlue: false })),
+  );
+};
+
+</script>
+
+<style lang="scss" scoped>
+.grid-container {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-rows: repeat(64, 36px);
+  background-color: #333;
+}
+
+.grid-square {
+  width: 36px;
+  height: 36px;
+  background-color: white;
+  border: 1px solid #111;
+}
+
+.grid-square.blue {
+  background-color: blue;
+}
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,102 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <v-card>
+        <v-toolbar
+          dark
+          color="primary"
+        >
+          <v-toolbar-title>Login form</v-toolbar-title>
+        </v-toolbar>
+
+        <v-card-text>
+          <v-form @submit.prevent="handleSubmit">
+            <v-text-field
+              v-model.triw="data.email"
+              label="Please enter your email"
+            />
+
+            <div
+              v-for="error of validator.email.$errors"
+              :key="error.$uid"
+              class="input-errors mb-4"
+            >
+              <div class="error-msg">
+                {{ error.$message }}
+              </div>
+            </div>
+
+            <v-text-field
+              v-model.triw="data.password"
+              type="password"
+              label="Please enter your password"
+            />
+
+            <div
+              v-for="error of validator.password.$errors"
+              :key="error.$uid"
+              class="input-errors mb-4"
+            >
+              <div class="error-msg">
+                {{ error.$message }}
+              </div>
+            </div>
+
+            <v-btn
+              type="submit"
+              block
+              class="mt-2"
+            >
+              Submit
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+import { useValidation } from '@/composables';
+import { reactive } from 'vue';
+import {
+  required,
+  email,
+  minLength,
+} from '@vuelidate/validators';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const data = reactive({
+  email: '',
+  password: '',
+});
+
+const validationRules = {
+  email: {
+    required,
+    email,
+    minLength: minLength(6),
+  },
+  password: {
+    required,
+    minLength: minLength(6),
+  },
+};
+
+const { validator } = useValidation(data, validationRules);
+
+const handleSubmit = () => {
+  validator.value.$touch();
+  if (!validator.value.$error) {
+    router.push('/game');
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.error-msg {
+  color: red;
+}
+</style>
