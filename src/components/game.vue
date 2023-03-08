768e450422b7582b91a02f5f9ef535254e03b01f
v_hovorun <volodymyr@nayaone.com>
2023-03-08 20:08:54 +0200

    Initial commit

--- src/components/game.vue
+++ src/components/game.vue
@@ -11,19 +11,19 @@
       label="Please enter size of y axis"
       @input="redrawSquares"
     />
-  </v-card>
 
-  <div
-    class="grid-container"
-    :style="{ gridTemplateColumns: `repeat(${sizeX}, 36px)` }"
-  >
     <div
-      v-for="(square, index) in squares"
-      :key="index"
-      :class="['grid-square', { 'blue': square.isBlue }]"
-      @mouseover="toggleSquareColor(square)"
-    />
-  </div>
+      class="grid-container"
+      :style="{ gridTemplateColumns: `repeat(${sizeX}, 36px)` }"
+    >
+      <div
+        v-for="(square, index) in squares"
+        :key="index"
+        :class="['grid-square', { 'blue': square.isBlue }]"
+        @mouseover="toggleSquareColor(square)"
+      />
+    </div>
+  </v-card>
 </template>
 
 <script setup lang="ts">
